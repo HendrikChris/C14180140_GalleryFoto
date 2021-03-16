8311abf1947f1d1e91fafb569966478c6461b517
HendrikChris <79140979+HendrikChris@users.noreply.github.com>
2021-03-16 21:10:03 +0700

    game

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  tebak = "";
+  asli: Number;
+  alamat = "https://tse4.mm.bing.net/th?id=OIP.VjGNlsCW2WxHS4fKSOScaAAAAA&pid=Api&P=0&w=163&h=156";
   constructor() {}
+
+  ngOnInit() {
+    this.asli = this.randomIntFromInterval(1, 5);
+    document.getElementById('correct_image').style.display = 'none';
+  }
+
+  randomIntFromInterval(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  cekSama(){
+    if (this.asli != Number(this.tebak)){
+      alert("Salah");
+    }
+    else{
+      alert("Benar");
+      document.getElementById('correct_image').style.display = 'block';
+    }
+    alert(this.asli)
+  }
 
 }
